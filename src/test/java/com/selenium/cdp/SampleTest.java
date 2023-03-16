ff9acf6470a16302a47e86a3b549f5d63b6aae46
premsundarraj <premksep4@gmail.com>
2023-03-16 13:23:12 +0530

    Prem | Added request validate Test

--- src/test/java/com/selenium/cdp/SampleTest.java
+++ src/test/java/com/selenium/cdp/SampleTest.java
@@ -1,12 +1,37 @@
 package com.selenium.cdp;
 
+import org.openqa.selenium.devtools.v85.fetch.Fetch;
 import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static com.selenium.cdp.NetworkIntercept.*;
 
 public class SampleTest extends BaseTest{
 
     @Test
     public void validateRequestIntercept(){
-        // Add sample test here......
+        createDevToolSession();
+        enableFetch();
+
+        devTool.addListener(Fetch.requestPaused(), request->
+        {
+            if(request.getRequest().getUrl().contains("entries"))
+            {
+                String mockURL = request.getRequest().getUrl().replace("entries","viewcart");
+                System.out.println(mockURL);
+
+                devTool.send(Fetch.continueRequest(request.getRequestId(), Optional.of(mockURL), Optional.empty()
+                        , Optional.empty(), Optional.empty()));
+            }
+            else
+            {
+                devTool.send(Fetch.continueRequest(request.getRequestId(), Optional.empty(), Optional.empty()
+                        , Optional.empty(), Optional.empty()));
+            }
+
+        });
+        driver.get("https://www.demoblaze.com/");
 
     }
 }
