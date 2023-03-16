f084f84596bb4f1cf79661be12cb47279b718e16
PremKumarSundarraj <prem.sundarraj@kroger.com>
2023-03-16 12:16:58 +0530

    Prem | Added Base Test and Network Util

--- src/test/java/com/selenium/cdp/NetworkIntercept.java
+++ src/test/java/com/selenium/cdp/NetworkIntercept.java
@@ -1,4 +1,27 @@
 package com.selenium.cdp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v85.fetch.Fetch;
+
+import java.util.Optional;
+
 public class NetworkIntercept {
+    protected static DevTools devTool;
+
+    public void createDevToolSession(){
+        WebDriver driver = BaseTest.getDriver();
+        devTool = ((ChromeDriver) driver).getDevTools();
+        devTool.createSession();
+    }
+
+    public void enableFetch(){
+        devTool.send(Fetch.enable(Optional.empty(),Optional.empty()));
+    }
+
+
+
+
 }
---
+++ src/test/java/com/selenium/cdp/SampleTest.java
@@ -0,0 +1,11 @@
+package com.selenium.cdp;
+
+import org.testng.annotations.Test;
+
+public class SampleTest extends BaseTest{
+
+    @Test
+    public void validateRequestIntercept(){
+
+    }
+}
